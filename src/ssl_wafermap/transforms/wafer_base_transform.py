4a84bdeb6eb949361079c1d3c8aa96ecb4c0a213
Faris Khan <khanmohammedfaris@gmail.com>
2023-06-06 12:28:18 -0600

    Refactor and simplify transforms
    Create a simplified BaseTransform and MultiCropTransform for multi view augmentations

--- src/ssl_wafermap/transforms/wafer_base_transform.py
+++ src/ssl_wafermap/transforms/wafer_base_transform.py
@@ -1,48 +1,35 @@
-from typing import List, Union
+from typing import List
 
 from lightly.transforms.multi_view_transform import MultiViewTransform
-from PIL.Image import Image
-from torch import Tensor
 
 from ssl_wafermap.transforms.augmentations import get_base_transforms
 
-# Notation convention:
-# {Model}ViewTransform refers to a transformation of a single view for a given model
-# {Model}Transform refers to the multi-view transform to be passed to a dataset for a given model, i.e. num_views {Model}ViewTransform
-
-# FastSiam can use BaseTransform with n_views=4, MAE with n_views=1
-
-
-class BaseViewTransform:
-    def __init__(
-        self,
-        img_size: List[int] = [224, 224],
-        die_noise_prob: float = 0.03,
-        crop: bool = False,
-        denoise: bool = False,
-        hf_prob: float = 0.5,
-        vf_prob: float = 0.5,
-        rr_prob: float = 0.5,
-        normalize: bool = True,
-    ):
-
-        self.transform = get_base_transforms(
-            img_size=img_size,
-            die_noise_prob=die_noise_prob,
-            denoise=denoise,
-            crop=crop,
-            hf_prob=hf_prob,
-            vf_prob=vf_prob,
-            rr_prob=rr_prob,
-            to_tensor=True,
-            normalize=normalize,
-        )
-
-    def __call__(self, image: Union[Tensor, Image]) -> Tensor:
-        return self.transform(image)
-
 
 class BaseTransform(MultiViewTransform):
+    """Base transform for wafermap images.
+
+    Parameters
+    ----------
+    img_size : List[int], optional
+        Size of the wafer map after resizing to a square shape, by default [224, 224]
+    die_noise_prob : float, optional
+        Probability of adding die noise, by default 0.03
+    crop : bool, optional
+        Whether to perform randomized cropping, by default False
+    denoise : bool, optional
+        Whether to perform median filter denoising, by default False
+    hf_prob : float, optional
+        Probability of horizontal flip, by default 0.5
+    vf_prob : float, optional
+        Probability of vertical flip, by default 0.5
+    rr_prob : float, optional
+        Probability of random rotation, by default 0.5
+    normalize : bool, optional
+        Whether to normalize the image, by default True
+    n_views : int, optional
+        Number of views to generate, by default 2
+    """
+
     def __init__(
         self,
         img_size: List[int] = [224, 224],
@@ -55,16 +42,17 @@ class BaseTransform(MultiViewTransform):
         normalize: bool = True,
         n_views: int = 2,
     ):
-
-        view_transform = BaseViewTransform(
+        view_transform = get_base_transforms(
             img_size=img_size,
             die_noise_prob=die_noise_prob,
-            crop=crop,
             denoise=denoise,
+            crop=crop,
             hf_prob=hf_prob,
             vf_prob=vf_prob,
             rr_prob=rr_prob,
+            to_tensor=True,
             normalize=normalize,
         )
         transforms = [view_transform] * n_views
+
         super().__init__(transforms=transforms)
